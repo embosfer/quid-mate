26656b4d4207d39df8a77a1135f31a0f416bb773
embosfer <e.bosch.ferrando@gmail.com>
2017-07-30 16:46:40 +0100

    Add Description's equals method

--- src/main/java/com/embosfer/quidmate/core/model/Description.java
+++ src/main/java/com/embosfer/quidmate/core/model/Description.java
@@ -16,6 +16,14 @@ public class Description {
     }
 
     @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Description)) return false;
+        Description other = (Description) obj;
+        return this.value.equals(other.value);
+    }
+
+    @Override
     public String toString() {
         return value;
     }

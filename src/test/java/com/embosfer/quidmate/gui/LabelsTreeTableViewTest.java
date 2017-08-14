5151f9c6e1fdccb4fc11815637bbbdf0c7be23e7
embosfer <e.bosch.ferrando@gmail.com>
2017-08-14 20:28:34 +0100

    Unit Test for LabelsTreeTableView

---
+++ src/test/java/com/embosfer/quidmate/gui/LabelsTreeTableViewTest.java
@@ -0,0 +1,24 @@
+package com.embosfer.quidmate.gui;
+
+import com.embosfer.quidmate.db.DbConnection;
+import org.junit.Test;
+
+import static org.mockito.Mockito.*;
+
+/**
+ * Created by embosfer on 14/08/2017.
+ */
+public class LabelsTreeTableViewTest {
+
+    DbConnection dbConnection = mock(DbConnection.class);
+
+    @Test
+    public void labelsGetLoadedFromDbLazilyWhenTheLabelTabIsClicked() {
+        LabelsTreeTableView labelsTreeTableView = new LabelsTreeTableView(dbConnection);
+        verify(dbConnection, times(0)).getAllLabels();
+
+        labelsTreeTableView.onTabClicked();
+
+        verify(dbConnection, times(1)).getAllLabels();
+    }
+}
